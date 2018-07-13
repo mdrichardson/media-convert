6eca73de4fe68af81f83928aefe2361d93591d65
Michael Richardson <michaelgocougs@gmail.com>
2018-07-13 10:21:48 -0700

    first commit

---
+++ ConvertMovies.ps1
@@ -0,0 +1,35 @@
+$filelist = Get-ChildItem "M:\TV & Movies\Movies" -include *.wmv,*.avi,*.mp4,*.mkv,*.mpg,*.m4v,*.mpeg,*.mov -recurse
+ 
+$num = $filelist | measure
+$filecount = $num.count
+ 
+$i = 0;
+ForEach ($file in $filelist)
+{
+    $i++;
+    $oldfile = $file.DirectoryName + "\" + $file.BaseName + $file.Extension;
+	$newdirectory = "X:\TV & Movies\Movies\" + (get-item $file.DirectoryName).parent + "\" + (get-item $oldfile).Directory.Name +  "\"
+    $newfile =  $file.BaseName + " - 720p" + ".mp4";
+	$newcomplete = $newdirectory + $newfile
+			If (-not(Test-Path $newdirectory)){
+			New-Item $newdirectory -type directory}{}
+			If (-not(Test-Path $newcomplete)){
+    $progress = ($i / $filecount) * 100
+    $progress = [Math]::Round($progress,2)
+ 
+    Clear-Host
+    Write-Host -------------------------------------------------------------------------------
+	Write-Host "New File: $newfile"
+    Write-Host Handbrake Batch Encoding
+    Write-Host "Processing - $oldfile"
+    Write-Host "File $i of $filecount - $progress%"
+    Write-Host -------------------------------------------------------------------------------
+
+$host.ui.RawUI.WindowTitle = "$i of $filecount - $progress%"	
+	
+    Start-Process "H:\Program Files\HandBrake\HandBrakeCLI.exe" -ArgumentList "-i `"$oldfile`" -t 1 --angle 1 -c 1 -o `"$newcomplete`" --optimize --format mp4 --ab 64 --mixdown mono --quality 23 -e x264 -x b-adapt=2:rc-lookahead=50:me=umh:bframes=5:ref=6:direct=auto:trellis=2:subq=10:psy-rd=1.0,0.10:analyse=all --width 1280 --height 720" -Wait -NoNewWindow
+}}
+Else{
+	Write-Host "Exists"
+	}
+	
---
+++ CopyMovieSubtitles.ps1
@@ -0,0 +1,41 @@
+$filelist = Get-ChildItem "M:\TV & Movies\Movies" -include *.sub,*.srt -recurse
+ 
+$num = $filelist | measure
+$filecount = $num.count
+ 
+$i = 0;
+ForEach ($file in $filelist)
+{
+    $i++;
+    $oldfile = $file.DirectoryName + "\" + $file.BaseName + $file.Extension;
+	$newdirectory = "X:\TV & Movies\Movies\" + (get-item $file.DirectoryName).parent + "\" + (get-item $oldfile).Directory.Name +  "\"
+	$oldfile.replace(".en","")
+	$oldfile.replace(" - 1080p","")
+	$oldfile.replace("- 1080p","")
+	$oldfile.replace("-1080p","")
+	$oldfile.replace("-DVD-Rip","")
+	$oldfile.replace("-BR-Rip","")
+    $newfile =  $file.BaseName + " - 720p.en" + $file.Extension;
+	$newcomplete = $newdirectory + $newfile
+			If (-not(Test-Path $newdirectory)){
+			New-Item $newdirectory -type directory}{}
+			If (-not(Test-Path $newcomplete)){
+    $progress = ($i / $filecount) * 100
+    $progress = [Math]::Round($progress,2)
+ 
+    Clear-Host
+    Write-Host -------------------------------------------------------------------------------
+	Write-Host "New File: $newfile"
+    Write-Host Handbrake Batch Encoding
+    Write-Host "Processing - $oldfile"
+    Write-Host "File $i of $filecount - $progress%"
+    Write-Host -------------------------------------------------------------------------------
+
+$host.ui.RawUI.WindowTitle = "$i of $filecount - $progress%"	
+	
+    Copy-Item $oldfile $newcomplete
+}}
+Else{
+	Write-Host "Exists"
+	}
+	
